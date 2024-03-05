f65521f94fafdcf202476c5102a9503e1ea8e09c
Keishibuya <139360166+Kei0501@users.noreply.github.com>
2024-03-05 21:36:22 +0900

    Update commons.py

--- src/commons.py
+++ src/commons.py
@@ -1,1 +1,20 @@
+import numpy as np
+
+def get_time_voltage_current_currindex0(nwb):
+    df = nwb.sweep_table.to_dataframe()
+    voltage = np.zeros((len(df['series'][0][0].data[:]), int((df.shape[0]+1)/2)))
+    time = np.arange(len(df['series'][0][0].data[:]))/df['series'][0][0].rate
+    voltage[:, 0] = df['series'][0][0].data[:]
+    current_initial = df['series'][1][0].data[12000]*df['series'][1][0].conversion
+    curr_index_0 = int(-current_initial/20) # index of zero current stimulation
+    current = np.linspace(current_initial, (int((df.shape[0]+1)/2)-1)*20+current_initial, \
+                         int((df.shape[0]+1)/2))
+    for i in range(curr_index_0):   # Find all voltage traces from minimum to 0 current stimulation
+        voltage[:, i+1] = df['series'][0::2][(i+1)*2][0].data[:]
+    for i in range(curr_index_0, int((df.shape[0]+1)/2)-1):   # Find all voltage traces from 0 to highest current stimulation
+        voltage[:, i+1] = df['series'][1::2][i*2+1][0].data[:]
+    voltage[:, curr_index_0] = df.loc[curr_index_0*2][0][0].data[:]    # Find voltage trace for 0 current stimulation
+    return time, voltage, current, curr_index_0
+
+
 
