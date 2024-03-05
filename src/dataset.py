f06bc8fb056898d35eb6a34c03a73151bdda4aad
Keishibuya <139360166+Kei0501@users.noreply.github.com>
2024-03-05 17:50:09 +0900

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,51 @@
+import torch
+import numpy as np
 
+class VaeDataSet(torch.utils.data.Dataset):
+    def __init__(self,x,xcell_id,transform=None,pre_transform=None):
+        self.x = x
+        self.xcell_id = xcell_id
+        self.transform = transforms.Compose([transforms.ToTensor()])
+
+    def __len__(self):
+        return(self.x.shape[0])
+      
+    def __getitem__(self,idx):
+        idx_x = self.x[idx]
+        idx_xcell_id = np.load(self.xcell_id[idx])
+        idx_xcell_id = np.array(Image.fromarray(idx_xcell_id).resize((set_timeax,set_freqax)))
+        idx_xcell_id = self.transform(idx_xcell_id)
+        idx_xcell_name = self.xcell_id[idx]
+        return(idx_x,idx_xcell_id,idx_xcell_name)
+
+    def normalize(self,img):
+        return (img-img.min())/(img.max()-img.min())
+
+
+class zDataSet(torch.utils.data.Dataset):
+    def __init__(self,x,xcell_id,transform=None,pre_transform=None):
+        self.x = x
+        self.xcell_id = xcell_id
+        self.transform = transforms.Compose([transforms.ToTensor()])
+      
+    def __len__(self):
+        return(self.x.shape[0])
+      
+    def __getitem__(self,idx):
+        idx_x = self.x[idx]
+        qz_mu, qz_logvar = t_model.enc_z(idx_x)
+        qz_logvar = t_model.softplus(qz_logvar)
+        qz = dist.Normal(qz_mu, qz_logvar)
+        t_z = qz.rsample()
+        idx_xcell_id = np.load(self.xcell_id[idx])
+        idx_xcell_id = np.array(Image.fromarray(idx_xcell_id).resize((set_timeax,set_freqax)))
+        idx_xcell_id = self.transform(idx_xcell_id)
+        qz_mu, qz_logvar = e_model.enc_z(idx_xcell_id)
+        qz_logvar = e_model.softplus(qz_logvar)
+        qz = dist.Normal(qz_mu, qz_logvar)
+        e_z = qz.rsample()
+        e_z = e_z.squeeze()
+        return(t_z,e_z)
+
+    def normalize(self,img):
+        return (img-img.min())/(img.max()-img.min())
