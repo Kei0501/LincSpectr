b2f020fef55f533a2bf24098ae4340d2fa1679e1
Keishibuya <139360166+Kei0501@users.noreply.github.com>
2024-03-06 13:41:09 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -80,6 +80,30 @@ def make_features(adata, data_path, file_path, extract_part):
   return(count_mat, cell_id)
 
 
+def get_time_voltage_current_currindex0(nwb):
+    df = nwb.sweep_table.to_dataframe()
+    voltage = np.zeros((len(df['series'][0][0].data[:]), int((df.shape[0]+1)/2)))
+    time = np.arange(len(df['series'][0][0].data[:]))/df['series'][0][0].rate
+    voltage[:, 0] = df['series'][0][0].data[:]
+    current_initial = df['series'][1][0].data[12000]*df['series'][1][0].conversion
+    curr_index_0 = int(-current_initial/20) # index of zero current stimulation
+    current = np.linspace(current_initial, (int((df.shape[0]+1)/2)-1)*20+current_initial, \
+                         int((df.shape[0]+1)/2))
+    for i in range(curr_index_0):   # Find all voltage traces from minimum to 0 current stimulation
+        voltage[:, i+1] = df['series'][0::2][(i+1)*2][0].data[:]
+    for i in range(curr_index_0, int((df.shape[0]+1)/2)-1):   # Find all voltage traces from 0 to highest current stimulation
+        voltage[:, i+1] = df['series'][1::2][i*2+1][0].data[:]
+    voltage[:, curr_index_0] = df.loc[curr_index_0*2][0][0].data[:]    # Find voltage trace for 0 current stimulation
+    return time, voltage, current, curr_index_0
+
+
+def plot_volatge(voltage):
+    plt.plot(voltage[:,pick_voltage])    
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['top'].set_visible(False)    
+    plt.show
+
+
 def embed_z(z, n_neighbors, min_dist):
     reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist)
     z_embed = reducer.fit_transform(z.cpu().detach().numpy())
