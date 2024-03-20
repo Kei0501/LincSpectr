2f1d8d6434ea022440b96c604e33e33a5bb58d8a
Kei0501 <139360166+Kei0501@users.noreply.github.com>
2024-03-20 17:11:15 +0900

    Update workflow.py

--- src/LincSpectr/workflow.py
+++ src/LincSpectr/workflow.py
@@ -1,16 +1,35 @@
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
 import utils
 from dataset import VaeDataSet, ZDataSet
 from modules import TscVAE, EscVAE, Linker
 from funcs import t_elbo_loss, e_elbo_loss
 
-file_names = collect_filename("./data_for_VAE/*")
-cell_list = collect_cellname(file_names)
-count_mat, cell_id = make_features(adata, './data_for_VAE/*.npy', './data_for_VAE/', "VAE/([^.]+)")
+#prepare transcriptome data
+#add RNA_family information and remove low quality data
+adata = sc.read_csv('./m1_patchseq_exon_counts.csv')
+plus_data = pd.read_table('./m1_patchseq_meta_data.csv',sep='\t',index_col=1)
+adata = adata.T
+adata.obs = plus_data.loc[adata.obs_names]
+adata = adata[adata.obs["RNA family"] != "low quality"]
+adata.layers['count'] = adata.X
+sc.pp.filter_cells(adata,min_counts=100)
+sc.pp.filter_genes(adata,min_cells=10)
+sc.pp.normalize_total(adata,target_sum=1e4)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata,n_top_genes=2000)
+
+file_names = utils.collect_filename("./data_for_VAE/*")
+cell_list = utils.collect_cellname(file_names)
+count_mat, cell_id, adata = utils.make_features(adata, './data_for_VAE/*.npy', './data_for_VAE/', "VAE/([^.]+)")
 set_timeax = 128
 set_freqax = 128
 dataset = VaeDataSet(count_mat, cell_id)
-val_x, val_xcell_id, val_xcell_names = split_dataset(dataset)
-valid_list = make_validlist(val_xcell_names)
+val_x, val_xcell_id, val_xcell_names, train_dataset,val_dataset,test_dataset = utils.split_dataset(dataset,test_ratio=0.05,val_ratio=0.1)
+valid_list = utils.make_validlist(val_xcell_names)
 
 # tVAE training
 x_dim = count_mat.size()[1]
@@ -22,7 +41,7 @@ t_vae.to(device)
 optimizer = torch.optim.Adam(t_vae.parameters(), lr=4.0e-3)
 
 for epoch in tqdm(range(epoch_num)):
-    tsc_vae.train()
+    t_vae.train()
     for x, xcell_id, xcell_names in loader:
         x = x.to(device)
         optimizer.zero_grad()
@@ -43,7 +62,7 @@ for epoch in tqdm(range(epoch_num)):
 i_dim = np.load(cell_id[0]).reshape(-1).shape[0]
 e_vae = EscVAE(i_dim, xz_dim=10, enc_z_h_dim=500, dec_z_h_dim=500, num_enc_z_layers=2, num_dec_z_layers=2)
 epoch_num = 300
-loader = torch.utils.data.DataLoader(train_dataset, batch_siz=16, shuffle=True, pin_memory=True)
+loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True, pin_memory=True)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 e_vae.to(device)
 optimizer = torch.optim.Adam(e_vae.parameters(), lr=1.0e-4)
@@ -66,10 +85,31 @@ for epoch in tqdm(range(epoch_num)):
             loss = e_elbo_loss(qz, ld_img, val_xcell_id.view(ld_img.size()))
             print(f'loss at epoch {epoch} is {loss}')
 
-zdataset = ZDataSet(count_mat,cell_id)
+zdataset = ZDataSet(count_mat,cell_id,t_vae,e_vae)
+test_ratio = 0.05
+val_ratio = 0.1
+
+total_size = len(zdataset)
+test_size = int(total_size * test_ratio)
+val_size = int(total_size * val_ratio)
+train_size = total_size - test_size - val_size
 train_zdataset,val_zdataset,test_zdataset = torch.utils.data.dataset.random_split(zdataset,[train_size,val_size,test_size],generator=torch.Generator().manual_seed(42))
-val_tz, val_ez, val_zname = split_dataset(val_zdataset,test_ratio, val_ratio)
-train_tz, train_ez, train_zname = split_dataset(train_zdataset,test_ratio, val_ratio)
+
+tz_features = []
+ez_features = []
+for i in range(len(val_zdataset)):
+    plus_tz = val_zdataset[i][0]
+    tz_features.append(plus_tz)
+    plus_ez = val_zdataset[i][1]
+    ez_features.append(plus_ez)
+val_tz = torch.stack(tz_features)
+val_ez = torch.stack(ez_features)
+
+ez_train_features = []
+for i in tqdm(range(len(train_zdataset))):
+    train_ez = train_zdataset[i][1]
+    ez_train_features.append(train_ez)
+val_ez_train = torch.stack(ez_train_features)
 
 # connection model training
 linkz_model = Linker(latent_dim=10)
